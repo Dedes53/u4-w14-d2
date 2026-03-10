530bc78dd50a5900cf06af2381cfa89609fe924a
Dedes <federico.lepore73@gmail.com>
2026-03-10 16:57:45 +0100

    fine es 4

--- src/esercizio4/Main.java
+++ src/esercizio4/Main.java
@@ -7,6 +7,42 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Inserire tempo per conto alla rovescia");
+        int num = scanner.nextInt();
 
+        if (num < 0) num = Math.abs(num);
+
+        if (num > 20) {
+            num = 20;
+            System.out.println("Ecceduto limite di sicurezza, forzato a 20 sec.");
+        }
+
+        System.out.println(countDownReport(num));
     }
+
+
+    static StringBuilder countDownReport(int num) {
+
+        StringBuilder report = new StringBuilder();
+        String inizio = "Secondi al lancio: " + num;
+        String sec10 = "--- SEPARAZIONE MODULO ---";
+        String sec0 = "--- IGNITION ---";
+
+        for (int i = num; i >= 0; i--) {
+
+            if (i == num) report.append(inizio);
+
+            String sec = i % 2 == 0 ? "[OK] " + i : "[CHECK] " + i;
+            if (i == 0) sec = sec0;
+            report.append('\n').append(sec);
+
+            if (i == 10) report.append('\n').append(sec10);
+
+
+        }
+
+        return report;
+    }
+
+
 }
